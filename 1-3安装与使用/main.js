0d0f36221370739901bf54352d355c0362bbae6d
Stphesun Sun <szy1944@hotmail.com>
2023-02-20 02:54:01 +0800

    改名字

--- 1-3安装与使用/main.js
+++ 1-3安装与使用/main.js
@@ -1,4 +1,12 @@
+/*
+ * @Author: Stphesun Sun szy1944@hotmail.com
+ * @Date: 2022-07-24 10:53:26
+ * @LastEditors: Stphesun Sun szy1944@hotmail.com
+ * @LastEditTime: 2023-02-20 02:53:39
+ * @FilePath: /dive-into-webpack/1-3安装与使用/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
 // 执行 show 函数
-show('Webpack');
+show('Webpack，改了');
