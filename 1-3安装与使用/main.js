b900cf957b35948984a839aaac3ac1aad82e354d
Stphesun Sun <szy1944@hotmail.com>
2023-02-20 09:30:04 +0800

    change spell

--- 1-3安装与使用/main.js
+++ 1-3安装与使用/main.js
@@ -1,8 +1,8 @@
 /*
- * @Author: Stphesun Sun szy1944@hotmail.com
+ * @Author: Stephen Sun szy1944@hotmail.com
  * @Date: 2022-07-24 10:53:26
- * @LastEditors: Stphesun Sun szy1944@hotmail.com
- * @LastEditTime: 2023-02-20 02:53:39
+ * @LastEditors: Stephen Sun szy1944@hotmail.com
+ * @LastEditTime: 2023-02-20 09:28:22
  * @FilePath: /dive-into-webpack/1-3安装与使用/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
